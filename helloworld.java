b58a3c2763f26f3c396c1265a6b1127e7e3157fe
Alex <62476175+asiemiat@users.noreply.github.com>
2020-04-08 18:55:12 +0200

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+class Hello_World{
+	
+	public static void main (String[] args){
+		
+		String name = "Alex";
+		
+		System.out.println("Hello World ~ " + name);
+		System.out.println("GitHub profile: https://github.com/asiemiat");
+		
+	}
+	
+	
+}
